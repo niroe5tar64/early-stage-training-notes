bc4a4996c06e1e745ac99b51f3131b9e5abc138a
niroe5tar64 <niroe5tar64@gmail.com>
2024-09-23 05:33:29 +0900

    add: calendarにDraftCodeを追加（初期表示時に現在日時から算出した表示年月要素を動的に表示）

---
+++ ishikawa-yuki/sample-code/calendar/01_initial-display-current-month/draft-code/main.js
@@ -0,0 +1,8 @@
+const date = new Date();
+const year = date.getFullYear();
+const month = date.getMonth() + 1;
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".date-display").innerHTML =
+    year + "年 " + month + "月";
+});
